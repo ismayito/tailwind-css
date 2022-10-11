3aafd1d739ca930c98d3003fe334034d58f1e855
ismayito <47735113+ismayito@users.noreply.github.com>
2022-10-11 11:25:29 +0300

    customizing tailwind css

--- App.jsx
+++ App.jsx
@@ -16,11 +16,11 @@ export default function App() {
               src="/img/beach-work.jpg"
               alt="Woman workcationing on the beach"
             />
-            <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+            <h1 className="mt-6 text-2xl font-bold font-headline text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
               You can work from anywhere.
               <br className="hidden lg:inline" /> <span className="text-indigo-500">Take advantage of it.</span>
             </h1>
-            <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+            <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl brand-dark">
               Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when you're not
               on vacation.
             </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,27 @@
 module.exports = {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
-  theme: {
-    extend: {
+  variant:{
+    extend:{
       backgroundColor:["active"]
+    }
+  },
+  theme: {
+
+    extend: {
+      colors:{
+        brand:{
+          DEFAULT:"0fa9e6",
+          light:"3fbaeb",
+          dark:"0c87b8"
+        }
+      },
+      fontFamily:{
+        headline:"Helvetica Neue"
+
+      }
+      
+      
     },
   },
   plugins: [],
